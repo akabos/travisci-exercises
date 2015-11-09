510cdf7f068962ef86c176f748e73168ef85a09c
Mikhail Lukyanchenko <ml@akabos.com>
2015-11-09 11:30:17 +0300

    pytest

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, Command
+
+
+class RunTests(Command):
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import subprocess
+        import sys
+        errno = subprocess.call([sys.executable, 'run_tests.py'])
+        raise SystemExit(errno)
+
 
 setup(
     name='example',
@@ -10,12 +28,13 @@ setup(
     url='http://example.com/',
     license='LICENSE',
     description='TravisCI configuration excersises',
+    cmdclass = {'test': RunTests},
     install_requires=[
         "numpy",
         "scipy",
     ],
     tests_require=[
-        'nose',
+        'pytest',
     ],
-    test_suite='nose.collector',
+    test_suite="tests",
 )
